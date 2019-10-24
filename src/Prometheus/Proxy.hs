39512cf5968f15b6a9b31de10be5340e172de6ca
Jesse Kempf <jessekempf@users.noreply.github.com>
2019-10-24 11:10:13 -0700

    Support logging Conduit-level exceptions to syslog or stderr (#4)

--- src/Prometheus/Proxy.hs
+++ src/Prometheus/Proxy.hs
@@ -7,21 +7,25 @@
 module Prometheus.Proxy where
 
 import           Control.Exception         (SomeException)
+import           Control.Monad.Catch       (MonadThrow, throwM)
 import           Control.Monad.IO.Class    (liftIO)
 import           Control.Monad.IO.Unlift   (MonadUnliftIO)
 import           Data.Conduit
+import           Network.HTTP.Client       (HttpException (..),
+                                            HttpExceptionContent (..))
 import           Network.HTTP.Types.Status (notFound404, ok200)
 import           Network.Wai.Conduit
 import           Network.Wai.Handler.Warp  hiding (Port)
 import           Prelude                   hiding (lines, map)
 import           Prometheus.Conduit
 import           Prometheus.Format.Type    (PrometheusLine)
+import           System.Log.Logger         (Priority (..), logM)
 
 
 data PrometheusProxy = PrometheusProxy {
   ppPort            :: Port,
   ppSources         :: [PrometheusLocation],
-  ppHandleException :: forall i o m. MonadUnliftIO m => ConduitT i o m () -> ConduitT i o m (),
+  ppHandleException :: forall i o m. (MonadUnliftIO m, MonadThrow m) => ConduitT i o m () -> ConduitT i o m (),
   ppRewriteRules    :: forall m. Monad m => ConduitT PrometheusLine PrometheusLine m ()
 }
 
@@ -48,3 +52,26 @@ onExceptionSkip = handleC (\(_ :: SomeException) -> return ())
 
 onExceptionPrint :: MonadUnliftIO m => ConduitT i o m () -> ConduitT i o m ()
 onExceptionPrint = handleC (\(e :: SomeException) -> liftIO $ print e)
+
+onExceptionLog :: (MonadUnliftIO m, MonadThrow m) => ConduitT i o m () -> ConduitT i o m ()
+onExceptionLog = handleC handleUnknownError . handleC handleParseError . handleC handleHttpError
+  where
+    handleParseError (PrometheusParseError loc badstr _) =
+      liftIO $
+        logM "Connector.Parser" WARNING ("Invalid line " <> show badstr <> " received from " <> show loc)
+
+    handleHttpError (HttpExceptionRequest req hec) =
+      case locationFromRequest req of
+        Nothing -> rethrow
+        Just loc -> liftIO . logM "Connector.HTTP" ERROR $
+                      case hec of
+                        ConnectionFailure exc -> "Could not connect to " <> show loc <> ": " <> show exc
+                        other -> "HTTP exception from " <> show loc <> ": " <> show other
+      where
+        rethrow = throwM (HttpExceptionRequest req hec)
+    handleHttpError (InvalidUrlException url reason) =
+      liftIO $ logM "Connector.HTTP" ERROR ("Invalid URL " <> url <> ": " <> reason)
+
+    handleUnknownError (exc :: SomeException) = do
+      liftIO $ logM "Connector" CRITICAL ("Fatal exception while proxying: " <> show exc)
+      throwM exc
